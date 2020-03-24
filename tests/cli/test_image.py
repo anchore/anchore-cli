f451aa2608ec58ffe0c6e79965d8d0eb302e126f
Alfredo Deza <adeza@anchore.com>
2020-03-24 16:49:34 -0400

    tests: vuln image actions are now tested
    Signed-off-by: Alfredo Deza <adeza@anchore.com>
    Signed-off-by: Josiah Ritchie <ritchie.josiah@solute.us>

--- tests/cli/test_image.py
+++ tests/cli/test_image.py
@@ -1,1 +1,120 @@
+import pytest
 from anchorecli.cli import image
+from click.testing import CliRunner
+
+
+@pytest.fixture
+def response(monkeypatch):
+    error = {
+        'success': False,
+        'payload': {},
+        'error': {
+            'detail': {'error_codes': []},
+            'httpcode': 404,
+            'message': 'image is not analyzed - analysis_status: analyzing'
+        },
+        'httpcode': 404,
+    }
+    ok = {
+        'success': True, 'httpcode': 200,
+        'payload': {
+            'imageDigest': 'sha256:0c03ccebef8d908f181a9fbd11eaf84c858be8396c71c89bf1b372ee59852eca',
+            'vulnerabilities': [{
+                'advisory_data': {'cves': ['CVE-2018-19801']},
+                'feed': 'vulnerabilities',
+                'feed_group': 'github:python',
+                'fix': '0.4.9',
+                'nvd_data': [],
+                'package': 'aubio-0.4.8',
+                'package_cpe': 'None',
+                'package_cpe23': 'None',
+                'package_name': 'aubio',
+                'package_path': '/usr/local/lib64/python3.6/site-packages/aubio',
+                'package_type': 'python',
+                'package_version': '0.4.8',
+                'severity': 'High',
+                'url': 'https://github.com/advisories/GHSA-7vvr-h4p5-m7fh',
+                'vendor_data': [],
+                'vuln': 'GHSA-7vvr-h4p5-m7fh'}
+            ],
+            'vulnerability_type': 'all'},
+        'error': {}
+    }
+
+    def apply(success=True, httpcode=200, has_error=None):
+        if success:
+            patch = lambda *a, **kw: ok
+        else:
+            if has_error:
+                error['error'] = has_error
+            patch = lambda *a, **kw: error
+
+        monkeypatch.setattr(image.anchorecli.clients.apiexternal, 'query_image', patch)
+    return apply
+
+
+headers = [
+    'Vulnerability ID',
+    'Package',
+    'Severity',
+    'Fix',
+    'CVE Refs',
+    'Vulnerability URL',
+    'Type',
+    'Feed Group',
+    'Package Path',
+]
+
+vulnerability = [
+    'GHSA-7vvr-h4p5-m7fh',
+    'aubio-0.4.8',
+    'High',
+    '0.4.9',
+    'https://github.com/advisories/GHSA-7vvr-h4p5-m7fh',
+    'python',
+    'github:python',
+    '/usr/local/lib64/python3.6/site-packages/aubio',
+]
+
+
+class TestQueryVuln:
+
+    def test_is_analyzing(self, monkeypatch, response):
+        monkeypatch.setattr(image.anchorecli.cli.utils, 'discover_inputimage', lambda *a, **kw: (None, None, '<digest>'))
+        monkeypatch.setattr(image, 'config', {'jsonmode': False})
+        runner = CliRunner()
+        response(success=False)
+        result = runner.invoke(image.query_vuln, ['centos/centos:8', 'all'])
+        assert result.exit_code == 100
+
+    def test_not_yet_analyzed(self, monkeypatch, response):
+        monkeypatch.setattr(image.anchorecli.cli.utils, 'discover_inputimage', lambda *a, **kw: (None, None, '<digest>'))
+        monkeypatch.setattr(image, 'config', {'jsonmode': False})
+        runner = CliRunner()
+        response(success=False, has_error={
+            'detail': {'error_codes': []},
+            'httpcode': 404,
+            'message': 'image is not analyzed - analysis_status: not_analyzed'
+        })
+        result = runner.invoke(image.query_vuln, ['centos/centos:8', 'all'])
+        assert result.exit_code == 101
+
+    @pytest.mark.parametrize('item', headers)
+    def test_success_headers(self, monkeypatch, response, item):
+        monkeypatch.setattr(image.anchorecli.cli.utils, 'discover_inputimage', lambda *a, **kw: (None, None, '<digest>'))
+        monkeypatch.setattr(image, 'config', {'jsonmode': False})
+        runner = CliRunner()
+        response(success=True)
+        result = runner.invoke(image.query_vuln, ['centos/centos:8', 'all'])
+        assert result.exit_code == 0
+        assert item in result.stdout
+
+    @pytest.mark.parametrize('item', vulnerability)
+    def test_success_info(self, monkeypatch, response, item):
+        monkeypatch.setattr(image.anchorecli.cli.utils, 'discover_inputimage', lambda *a, **kw: (None, None, '<digest>'))
+        monkeypatch.setattr(image, 'config', {'jsonmode': False})
+        runner = CliRunner()
+        response(success=True)
+        result = runner.invoke(image.query_vuln, ['centos/centos:8', 'all'])
+        assert result.exit_code == 0
+        assert item in result.stdout
