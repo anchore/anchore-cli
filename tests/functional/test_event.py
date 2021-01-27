4fd83acf88d3bdd17bc3aa8f9b847e5b488547f3
Toure Dunnon <toure.dunnon@anchore.com>
2021-01-26 19:51:06 -0500

    fixed: stubbed out event test
    Signed-off-by: Toure Dunnon <toure.dunnon@anchore.com>

--- tests/functional/test_event.py
+++ tests/functional/test_event.py
@@ -10,9 +10,16 @@ def event_command(admin_call):
     def apply(sub_command, flags):
         output, _, _ = admin_call(["--json", "event", sub_command] + flags)
         return json.loads(output)
+
     return apply
 
 
 class TestEvents:
+    def test_event_list(self):
+        pass
 
-    def test_
+    def test_event_get(self):
+        pass
+
+    def test_event_delete(self):
+        pass
